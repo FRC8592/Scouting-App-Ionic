5446ed29be2e10c99e2fa3e2d5105d0d1582ed20
fireyinc <aryantadepalli50@yahoo.com>
2023-04-01 14:47:19 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scouting-app-but-ionic',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
